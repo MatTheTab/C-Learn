a4bd6ef3383bf74949f432cf2734e72e5c5e22b4
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-03-23 16:22:29 +0100

    Adding layer class file

---
+++ Layer.cpp
@@ -0,0 +1,32 @@
+# include "Layer.h"
+
+//Layer constructor function
+Layer::Layer(int _num_neurons){
+	// Initializing the Layer with set number of neurons
+	this->num_neurons=_num_neurons
+	neurons = new neuron[_num_neurons];
+}
+
+//Layer destructor function
+Neuron::~Neuron(){
+	//Deleting neurons
+    delete[] neurons;
+}
+
+//Layer feedForwards() function
+double Neuron::weightedSum(double *inputs){
+	double weighted_sum;
+	for(int i=0;i<num_neurons;i++){
+		weighted_sum+=inputs[i]*weights[i]
+	}
+	return weighted_sum
+}
+
+//Layer printing weights function
+void Neuron::printWeights(){
+	std::cout<<"Printing Weights at Layer: "<<std::endl;
+	for(int i=0;i<num_neurons;i++){
+		//Printing Individual neuron weights
+		neurons[i].printWeights();
+	}
+}
