f6b5ac0bc0b2babadf9719952494a8614c59e922
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-03-28 14:12:58 +0200

    Expand activation functions tests

--- ActivationFunctionsTests.cpp
+++ ActivationFunctionsTests.cpp
@@ -16,9 +16,9 @@ int main() {
     double leaky_relu_outputs[] = {0.0, 1.0, 2.0, 3.0, -0.01, -0.075, 3.14}; //for alpha=0.01
     double sigmoid_outputs[] = {0.5, 0.731, 0.881, 0.952, 0.269, 0.0006, 0.958};
     double tanh_outputs[] = {0.0, 0.761, 0.964, 0.996, -0.761, -1.0, 0.998};
-    double elu_outputs[] = {0.0, 1.0, 2.0, 3.0, -0.01 * (exp(1.0) - 1), -0.01 * (exp(7.5) - 1), 3.14}; //for alpha=0.01
-	double swish_outputs[] = {0.0, 0.731, 1.761, 2.857, -0.268, -6.818, 2.842}; // for beta=1
-	double gelu_outputs[] = {0.0, 0.841, 1.954, 2.997, -0.158, -1.0, 2.998};
+    double elu_outputs[] = {0.0, 1.0, 2.0, 3.0, 0.01 * (exp(-1.0) - 1), 0.01 * (exp(-7.5) - 1), 3.14}; //for alpha=0.01
+	double swish_outputs[] = {0.0, 0.731, 1.761, 2.857, -0.268, -0.004, 3.017}; // for beta=1
+	double gelu_outputs[] = {0.0, 0.841, 1.954, 2.997, -0.158, 0.0, 3.138};
     
     //Testing loop
     for(int i=0;i<arr_length;i++){
@@ -27,9 +27,9 @@ int main() {
    		assert(isEqual(leaky_relu_outputs[i],leakyRelu(inputs[i])));
     	assert(isEqual(sigmoid_outputs[i],sigmoid(inputs[i])));
     	assert(isEqual(tanh_outputs[i],tanh_act(inputs[i])));
-    	//assert(isEqual(elu_outputs[i],elu(inputs[i])));
-    	//assert(isEqual(swish_outputs[i],swish(inputs[i])));
-    	//assert(isEqual(gelu_outputs[i],gelu(inputs[i])));
+    	assert(isEqual(elu_outputs[i],elu(inputs[i])));
+    	assert(isEqual(swish_outputs[i],swish(inputs[i])));
+    	assert(isEqual(gelu_outputs[i],gelu(inputs[i])));
 }
     return 0;
 }
