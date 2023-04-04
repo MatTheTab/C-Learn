4fd155bc39f714c3ca097d042e4f9b5700921ff7
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-04-04 11:33:52 +0200

    Extend prototype for Network header file

--- Network.h
+++ Network.h
@@ -5,11 +5,20 @@
 #include <vector>
 #include "Layer.h"
 
-class Layer{
+class Network{
 private:
-	//TODO: fill
+	std::vector<Layer> layers;
 public:
-	//TODO: fill
+	Network();
+	void append(Layer new_layer);
+	void append(std::vector<Layer> new_layers);
+	void append(Layer* new_layers, int num_new_layers);
+	void add(Layer new_layer, int position);
+	void add(std::vector<Layer> new_layers, int position);
+	void add(Layer* new_layers; int position);
+	void appendNetwork(Network new_network);
+	void addNetwork(Network new_network, int position);
+	void add(Layer );
 };
 
 #endif
