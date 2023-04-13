3720d94723502620496864fad10af31bea23f825
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-04-13 20:49:00 +0200

    Prepare NetworkTests for further work

---
+++ NetworkTest.cpp
@@ -0,0 +1,11 @@
+#include "Network.cpp"
+
+int main() {
+	Network* test_network= new Network();
+	Layer* input_layer=new Layer();
+	//TODO: uncomment and fill
+	//input_layer.setWeights();
+	//TODO: uncomment and fill
+	//Layer* new_layer=new Layer(starting_weights);
+	
+}
