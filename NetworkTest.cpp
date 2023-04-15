44f405f8239b59bfc832ec6477d03988ada0e319
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-04-15 22:48:43 +0200

    Work on Network Tests

--- NetworkTest.cpp
+++ NetworkTest.cpp
@@ -3,6 +3,14 @@
 int main() {
 	Network* test_network= new Network();
 	Layer* input_layer=new Layer();
+	test_network.append(input_layer);
+	Layer* layer_1=new Layer()
+	Layer* layer_2=new Layer()
+	Layer* layer_3=new Layer()
+	Layer* output_layer= new Layer()
+	std::vector<Layer*> layers = {layer_1, layer_2, layer_3};
+	test_network.append(layers);
+	test_network.append(output_layer);
 	//TODO: uncomment and fill
 	//input_layer.setWeights();
 	//TODO: uncomment and fill
