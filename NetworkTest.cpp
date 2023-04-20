0ce1cb9e355ac4ab47fd7ab25f23da1e85dec043
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-04-20 16:27:36 +0200

    Add TODO to NetworkTests

--- NetworkTest.cpp
+++ NetworkTest.cpp
@@ -2,9 +2,9 @@
 
 int main() {
 	Network* test_network= new Network();
-	test_network.createInputLayer(5);
-	int neurons[5] = {3,7,7,9,5};
+	test_network->createInputLayer(5);
+	long neurons[5] = {3,7,7,9,5};
 	Activation_Function functions[5] = {linear,relu,relu,tanh, tanh};
-	test_network.createLayers(neurons,functions);
-	test_network.createOutputLayer(2,linear,softmax);
+	test_network->createLayers(neurons,functions);
+	test_network->createOutputLayer(2,linear,softmax);
 }
