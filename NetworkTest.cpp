e19597ade975bef1907c7717f4e51d1dfa2c7f9c
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-04-20 15:41:41 +0200

    Rework NetworkTests

--- NetworkTest.cpp
+++ NetworkTest.cpp
@@ -2,18 +2,9 @@
 
 int main() {
 	Network* test_network= new Network();
-	Layer* input_layer=new Layer();
-	test_network.append(input_layer);
-	Layer* layer_1=new Layer()
-	Layer* layer_2=new Layer()
-	Layer* layer_3=new Layer()
-	Layer* output_layer= new Layer()
-	std::vector<Layer*> layers = {layer_1, layer_2, layer_3};
-	test_network.append(layers);
-	test_network.append(output_layer);
-	//TODO: uncomment and fill
-	//input_layer.setWeights();
-	//TODO: uncomment and fill
-	//Layer* new_layer=new Layer(starting_weights);
-	
+	test_network.createInputLayer(5);
+	int neurons[5] = {3,7,7,9,5};
+	Activation_Function functions[5] = {linear,relu,relu,tanh, tanh};
+	test_network.createLayers(neurons,functions);
+	test_network.createOutputLayer(2,linear,softmax);
 }
