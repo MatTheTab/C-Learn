09471822a7bc1fdc3c2cd84902f87dca2219fd40
Mateusz Tabaszewski <mateusztabaszewski@gmail.com>
2023-04-22 23:09:27 +0200

    Setup environment to work as a project

--- NetworkTest.cpp
+++ NetworkTest.cpp
@@ -1,11 +1,11 @@
-#include "Network.cpp"
-
+//#include "Network.cpp"
+  #include "Network.h"
 int testNetwork() {
 	//TODO: fill testing functionality
 	Network* test_network= new Network();
-	test_network.createInputLayer(5);
-	int neurons[5] = {3,7,7,9,5};
-	Activation_Function functions[5] = {linear,relu,relu,tanh, tanh};
-	test_network.createLayers(neurons,functions);
-	test_network.createOutputLayer(2,linear,softmax);
+	test_network->createInputLayer(5);
+	long neurons[] = {3,7,7,9,5};
+	Activation_Function functions[] = {linear,relu,relu,tanh_act, tanh_act};
+	test_network->createLayers(neurons,functions);
+	test_network->createOutputLayer(2,linear,softmax);
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+int testActivationFunctions();
+int testLayer();
+int testNeuron();
+int testNetwork();
+
 int main()
 {
+	testActivationFunctions();
+	testNeuron();
+	testLayer();
+	testNetwork();
+	int helper;
+	std::cin>>helper;
 	return 0;
 }
